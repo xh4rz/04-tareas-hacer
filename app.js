f90a8843815968d099d880f20e596152cd9ebbea
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-04-25 13:04:08 -0500

    47. stdin - stdout - Readline

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 require('colors');
+const { mostrarMenu, pausa } = require('./helpers/mensajes');
 
 console.clear();
 
 const main = async () => {
 	console.log('Hola Mundo');
+	mostrarMenu();
+	// pausa();
 };
 
 main();
