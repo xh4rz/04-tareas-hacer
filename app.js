e78cf7ae84e958757254e06e9e3c27cc84a7af79
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-04-27 02:12:48 -0500

    51. Opciones del menú interactivo

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 require('colors');
-const { inquirerMenu } = require('./helpers/inquirer');
+const { inquirerMenu, pausa } = require('./helpers/inquirer');
 
 console.clear();
 
@@ -11,6 +11,8 @@ const main = async () => {
 	do {
 		opt = await inquirerMenu();
 		console.log({ opt });
+
+		await pausa();
 	} while (opt !== '0');
 };
 
