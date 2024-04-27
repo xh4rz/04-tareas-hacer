4337fd0b654590d4d6ea0d43af66092951af49cc
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-04-27 02:29:28 -0500

    52. Lógica para el manejo de las tareas por hacer

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('colors');
 const { inquirerMenu, pausa } = require('./helpers/inquirer');
-
-console.clear();
+const Tarea = require('./models/tarea');
+const Tareas = require('./models/tareas');
 
 const main = async () => {
 	console.log('Hola Mundo');
@@ -9,8 +9,15 @@ const main = async () => {
 	let opt = '';
 
 	do {
-		opt = await inquirerMenu();
-		console.log({ opt });
+		// opt = await inquirerMenu();
+		// console.log({ opt });
+		const tareas = new Tareas();
+		const tarea = new Tarea('Comprar comida');
+
+		tareas._listado[tarea.id] = tarea;
+
+		console.log(tarea);
+		console.log(tareas);
 
 		await pausa();
 	} while (opt !== '0');
---
+++ models/tareas.js
@@ -0,0 +1,9 @@
+class Tareas {
+	_listado = {};
+
+	constructor() {
+		this._listado = {};
+	}
+}
+
+module.exports = Tareas;
